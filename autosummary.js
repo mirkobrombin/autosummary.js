234192eb432232214adc7ec31bdb4f52a09a6a63
Mirko Brombin <send@mirko.pm>
2020-08-01 17:51:23 +0200

    Create autosummary.js

---
+++ autosummary.js
@@ -0,0 +1,29 @@
+headings = document.querySelectorAll("main>article>div h2,main>article>div h3,main>article>div h4"),
+summary = document.querySelector("aside>ul"),
+document.querySelector("aside>ul>li:first-child").remove(),
+url = window.location.href,
+i=0,
+// generate summary entries
+headings.forEach(t => {
+    tt = 'title' + i.toString(),
+    t.setAttribute('id', tt),
+    li = document.createElement("li"),
+    a = document.createElement("a"),
+    a.classList.add(tt, t.tagName.toUpperCase()), a.setAttribute("href", url + '#' + tt), a.innerText = t.innerText
+    li.appendChild(a),
+    summary.appendChild(li),
+    ++i
+}),
+// on scroll active summary entry
+window.addEventListener('scroll', function(e) {
+    headings.forEach(t => {
+        if(window.scrollY >= (t.offsetTop - 50) & window.scrollY <= (t.offsetTop + 50)) {
+            try {
+                document.querySelector("aside>ul>li>a.active").classList.remove("active")
+            } catch (error) {
+                console.debug("No selected summary item found.")
+            }
+            document.querySelector('aside>ul>li>a.' + t.getAttribute("id")).classList.add("active")
+        }
+    })
+});
